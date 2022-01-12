419546cde1b8f679b16433162d7d31107e74f7cc
David Chavez <depch47@gmail.com>
2022-01-12 14:35:41 -0500

    added build folder for the frontend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import personsRouter from './src/routes/personsRoutes.js'
 const app = express()
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
